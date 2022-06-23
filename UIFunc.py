0f9661b414555a0fdaf0cfb6897468657aa0ace9
Monomux <Monomux@outlook.com>
2022-06-23 17:38:56 +0800

    fix:Cannot play sound

--- UIFunc.py
+++ UIFunc.py
@@ -479,10 +479,6 @@ class UIFunc(QMainWindow, Ui_UIView, QtStyleTools):
             scripts_map['choice_language'] = 'English'
         self.retranslateUi(self)
 
-    def onchangevolume(self):
-        self.playerstart.setVolume(self.volumeSlider.value()/100.0)
-        self.playerend.setVolume(self.volumeSlider.value()/100.0)
-
     def onchangetheme(self):
         self.apply_stylesheet(self.app, theme=self.choice_theme.currentText())
         self.config.setValue("Config/Theme", self.choice_theme.currentText())
