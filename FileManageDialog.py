ae864daff7dbebcd4ec982f171c3b6231176c70c
ZutJoe <zhout50@163.com>
2022-07-18 13:54:15 +0800

    file manage ui

---
+++ FileManageDialog.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'file_manage.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(262, 111)
+        self.formLayoutWidget = QWidget(Dialog)
+        self.formLayoutWidget.setObjectName(u"formLayoutWidget")
+        self.formLayoutWidget.setGeometry(QRect(10, 10, 241, 31))
+        self.formLayout = QFormLayout(self.formLayoutWidget)
+        self.formLayout.setObjectName(u"formLayout")
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.formLayoutWidget)
+        self.label.setObjectName(u"label")
+
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
+
+        self.lineEdit = QLineEdit(self.formLayoutWidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.lineEdit)
+
+        self.horizontalLayoutWidget = QWidget(Dialog)
+        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
+        self.horizontalLayoutWidget.setGeometry(QRect(10, 60, 241, 31))
+        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.pushButton_2 = QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.horizontalLayout.addWidget(self.pushButton_2)
+
+        self.pushButton = QPushButton(self.horizontalLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.horizontalLayout.addWidget(self.pushButton)
+
+        self.pushButton_3 = QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.horizontalLayout.addWidget(self.pushButton_3)
+
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"file name", None))
+        self.lineEdit.setText("")
+        self.pushButton_2.setText(QCoreApplication.translate("Dialog", u"choice", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"edit", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Dialog", u"rename", None))
+    # retranslateUi
+
