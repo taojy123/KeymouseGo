3303771373a6dd90072d4bb4b5722b0f5fecdd47
Monomux <Monomux@outlook.com>
2022-10-04 23:48:44 +0800

    check ts file

--- assets/i18n/en.ts
+++ assets/i18n/en.ts
@@ -7,31 +7,31 @@
         <location filename="UIFileDialogView.ui" line="14"/>
         <source>File Manage</source>
         <translatorcomment>File Manage</translatorcomment>
-        <translation type="unfinished">File Manage</translation>
+        <translation>File Manage</translation>
     </message>
     <message>
         <location filename="UIFileDialogView.ui" line="33"/>
         <source>file name</source>
         <translatorcomment>file name</translatorcomment>
-        <translation type="unfinished">file name</translation>
+        <translation>file name</translation>
     </message>
     <message>
         <location filename="UIFileDialogView.ui" line="59"/>
         <source>choice</source>
         <translatorcomment>Choice</translatorcomment>
-        <translation type="unfinished">Choice</translation>
+        <translation>Choice</translation>
     </message>
     <message>
         <location filename="UIFileDialogView.ui" line="66"/>
         <source>edit</source>
         <translatorcomment>Edit</translatorcomment>
-        <translation type="unfinished">Edit</translation>
+        <translation>Edit</translation>
     </message>
     <message>
         <location filename="UIFileDialogView.ui" line="73"/>
         <source>rename</source>
         <translatorcomment>Rename</translatorcomment>
-        <translation type="unfinished">Rename</translation>
+        <translation>Rename</translation>
     </message>
 </context>
 <context>
@@ -39,113 +39,113 @@
     <message>
         <location filename="UIView.ui" line="14"/>
         <source>KeymomuseGo v5.1</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="UIView.ui" line="31"/>
         <source>Hotkeys</source>
         <translatorcomment>Hotkeys</translatorcomment>
-        <translation type="unfinished">Hotkeys</translation>
+        <translation>Hotkeys</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="69"/>
         <source>Terminate</source>
         <translatorcomment>Terminate</translatorcomment>
-        <translation type="unfinished">Terminate</translation>
+        <translation>Terminate</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="76"/>
         <source>Language</source>
         <translatorcomment>Language</translatorcomment>
-        <translation type="unfinished">Language</translation>
+        <translation>Language</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="106"/>
         <source>Record/Pause</source>
         <translatorcomment>Record/Pause</translatorcomment>
-        <translation type="unfinished">Record/Pause</translation>
+        <translation>Record/Pause</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="123"/>
         <source>Extension</source>
         <translatorcomment>Extension</translatorcomment>
-        <translation type="unfinished">Extension</translation>
+        <translation>Extension</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="130"/>
         <source>Launch/Pause</source>
         <translatorcomment>Launch/Pause</translatorcomment>
-        <translation type="unfinished">Launch/Pause</translation>
+        <translation>Launch/Pause</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="158"/>
         <source>Record</source>
         <translatorcomment>Record</translatorcomment>
-        <translation type="unfinished">Record</translation>
+        <translation>Record</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="171"/>
         <source>Launch</source>
         <translatorcomment>Launch/Pause</translatorcomment>
-        <translation type="unfinished">Launch/Pause</translation>
+        <translation>Launch/Pause</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="187"/>
         <source>Pause Record</source>
         <translatorcomment>Record/Pause</translatorcomment>
-        <translation type="unfinished">Record/Pause</translation>
+        <translation>Record/Pause</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="206"/>
         <source>Ready...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="UIView.ui" line="241"/>
         <source>Volume</source>
         <translatorcomment>Volume</translatorcomment>
-        <translation type="unfinished">Volume</translation>
+        <translation>Volume</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="264"/>
         <source>Config</source>
         <translatorcomment>Config</translatorcomment>
-        <translation type="unfinished">Config</translation>
+        <translation>Config</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="292"/>
         <source>Mouse precision</source>
         <translatorcomment>Mouse precision</translatorcomment>
-        <translation type="unfinished">Mouse precision</translation>
+        <translation>Mouse precision</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="299"/>
         <source>Running speed(%)</source>
         <translatorcomment>Running speed(%)</translatorcomment>
-        <translation type="unfinished">Running speed(%)</translation>
+        <translation>Running speed(%)</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="306"/>
         <source>Run times</source>
         <translatorcomment>Run times</translatorcomment>
-        <translation type="unfinished">Run times</translation>
+        <translation>Run times</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="313"/>
         <source>Theme</source>
         <translatorcomment>Theme</translatorcomment>
-        <translation type="unfinished">Theme</translation>
+        <translation>Theme</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="320"/>
         <source>Script</source>
         <translatorcomment>Script</translatorcomment>
-        <translation type="unfinished">Script</translation>
+        <translation>Script</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="402"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
--- assets/i18n/zh-cn.ts
+++ assets/i18n/zh-cn.ts
@@ -7,31 +7,31 @@
         <location filename="UIFileDialogView.ui" line="14"/>
         <source>File Manage</source>
         <translatorcomment>文件管理</translatorcomment>
-        <translation type="unfinished">文件管理</translation>
+        <translation>文件管理</translation>
     </message>
     <message>
         <location filename="UIFileDialogView.ui" line="33"/>
         <source>file name</source>
         <translatorcomment>文件名称</translatorcomment>
-        <translation type="unfinished">文件名称</translation>
+        <translation>文件名称</translation>
     </message>
     <message>
         <location filename="UIFileDialogView.ui" line="59"/>
         <source>choice</source>
         <translatorcomment>选择文件</translatorcomment>
-        <translation type="unfinished">选择文件</translation>
+        <translation>选择文件</translation>
     </message>
     <message>
         <location filename="UIFileDialogView.ui" line="66"/>
         <source>edit</source>
         <translatorcomment>编辑文件</translatorcomment>
-        <translation type="unfinished">编辑文件</translation>
+        <translation>编辑文件</translation>
     </message>
     <message>
         <location filename="UIFileDialogView.ui" line="73"/>
         <source>rename</source>
         <translatorcomment>重命名</translatorcomment>
-        <translation type="unfinished">重命名</translation>
+        <translation>重命名</translation>
     </message>
 </context>
 <context>
@@ -39,113 +39,113 @@
     <message>
         <location filename="UIView.ui" line="14"/>
         <source>KeymomuseGo v5.1</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="UIView.ui" line="31"/>
         <source>Hotkeys</source>
         <translatorcomment>热键</translatorcomment>
-        <translation type="unfinished">热键</translation>
+        <translation>热键</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="69"/>
         <source>Terminate</source>
         <translatorcomment>终止录制/执行</translatorcomment>
-        <translation type="unfinished">终止录制/执行</translation>
+        <translation>终止录制/执行</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="76"/>
         <source>Language</source>
         <translatorcomment>语言</translatorcomment>
-        <translation type="unfinished">语言</translation>
+        <translation>语言</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="106"/>
         <source>Record/Pause</source>
         <translatorcomment>开始/暂停录制</translatorcomment>
-        <translation type="unfinished">开始/暂停录制</translation>
+        <translation>开始/暂停录制</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="123"/>
         <source>Extension</source>
         <translatorcomment>扩展插件</translatorcomment>
-        <translation type="unfinished">扩展插件</translation>
+        <translation>扩展插件</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="130"/>
         <source>Launch/Pause</source>
         <translatorcomment>暂停执行</translatorcomment>
-        <translation type="unfinished">暂停执行</translation>
+        <translation>暂停执行</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="158"/>
         <source>Record</source>
         <translatorcomment>开始录制</translatorcomment>
-        <translation type="unfinished">开始录制</translation>
+        <translation>开始录制</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="171"/>
         <source>Launch</source>
         <translatorcomment>启动</translatorcomment>
-        <translation type="unfinished">启动</translation>
+        <translation>启动</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="187"/>
         <source>Pause Record</source>
         <translatorcomment>暂停录制</translatorcomment>
-        <translation type="unfinished">暂停录制</translation>
+        <translation>暂停录制</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="206"/>
         <source>Ready...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="UIView.ui" line="241"/>
         <source>Volume</source>
         <translatorcomment>音量</translatorcomment>
-        <translation type="unfinished">音量</translation>
+        <translation>音量</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="264"/>
         <source>Config</source>
         <translatorcomment>配置</translatorcomment>
-        <translation type="unfinished">配置</translation>
+        <translation>配置</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="292"/>
         <source>Mouse precision</source>
         <translatorcomment>鼠标精度</translatorcomment>
-        <translation type="unfinished">鼠标精度</translation>
+        <translation>鼠标精度</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="299"/>
         <source>Running speed(%)</source>
         <translatorcomment>执行速度(%)</translatorcomment>
-        <translation type="unfinished">执行速度(%)</translation>
+        <translation>执行速度(%)</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="306"/>
         <source>Run times</source>
         <translatorcomment>执行次数</translatorcomment>
-        <translation type="unfinished">执行次数</translation>
+        <translation>执行次数</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="313"/>
         <source>Theme</source>
         <translatorcomment>主题</translatorcomment>
-        <translation type="unfinished">主题</translation>
+        <translation>主题</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="320"/>
         <source>Script</source>
         <translatorcomment>脚本</translatorcomment>
-        <translation type="unfinished">脚本</translation>
+        <translation>脚本</translation>
     </message>
     <message>
         <location filename="UIView.ui" line="402"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
