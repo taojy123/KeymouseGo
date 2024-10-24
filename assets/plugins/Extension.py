d1d372e73aa63e4402cc3eb7d296389d75dad135
rushionn <101056203+rushionn@users.noreply.github.com>
2024-10-24 15:00:17 +0800

    Update 簡>繁

--- assets/plugins/Extension.py
+++ assets/plugins/Extension.py
@@ -8,30 +8,30 @@ class Extension:
         self.thd = thd
         self.swap = swap
 
-    # 脚本执行前需要做的事
+    # 腳本執行前需要做的事
     def onbeginp(self):
         pass
 
-    # 每次录制事件后需要做的事,返回True保存事件
+    # 每次錄制事件後需要做的事,返回True保存事件
     def onrecord(self, event, currentindex):
         return True
 
-    # 每次脚本执行前需要做的事,返回True执行本行脚本
+    # 每次腳本執行前需要做的事,返回True執行本行腳本
     def onbeforeeachloop(self, currentloop):
         return True
 
-    # 每行脚本执行前需要做的事,返回True执行本行脚本
+    # 每行腳本執行前需要做的事,返回True執行本行腳本
     def onrunbefore(self, event, currentindex):
         return True
 
-    # 每行脚本执行后需要做的事
+    # 每行腳本執行後需要做的事
     def onrunafter(self, event, currentindex):
         pass
 
-    # 每次脚本全部执行完后需要做的事
+    # 每次腳本全部執行完後需要做的事
     def onaftereachloop(self, currentloop):
         pass
 
-    # 全部循环全部执行完后需要做的事
+    # 全部循環全部執行完後需要做的事
     def onendp(self):
         pass
