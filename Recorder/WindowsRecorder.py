d5b367e4cb80f34639981a13d0df6d1f7a30890e
rushionn <101056203+rushionn@users.noreply.github.com>
2024-10-24 15:30:32 +0800

    Update 簡>繁

--- Recorder/WindowsRecorder.py
+++ Recorder/WindowsRecorder.py
@@ -6,7 +6,7 @@ from loguru import logger
 import Recorder.globals as globalv
 import collections
 from winreg import QueryValueEx, OpenKey, HKEY_CURRENT_USER, KEY_READ
-# 是否切换主要/次要功能键
+# 是否切換主要/次要功能鍵
 swapmousebuttons = True if QueryValueEx(OpenKey(HKEY_CURRENT_USER,
                                                 r'Control Panel\Mouse',
                                                 0,
@@ -33,14 +33,14 @@ record_signals = globalv.RecordSignal()
 
 
 def on_mouse_event(event):
-    # print('MessageName:',event.MessageName)  #事件名称
+    # print('MessageName:',event.MessageName)  #事件名稱
     # print('Message:',event.Message)          #windows消息常量
-    # print('Time:',event.Time)                #事件发生的时间戳
+    # print('Time:',event.Time)                #事件發生的時間戳
     # print('Window:',event.Window)            #窗口句柄
-    # print('WindowName:',event.WindowName)    #窗口标题
-    # print('Position:',event.Position)        #事件发生时相对于整个屏幕的坐标
-    # print('Wheel:',event.Wheel)              #鼠标滚轮
-    # print('Injected:',event.Injected)        #判断这个事件是否由程序方式生成，而不是正常的人为触发。
+    # print('WindowName:',event.WindowName)    #窗口標題
+    # print('Position:',event.Position)        #事件發生時相對於整個屏幕的坐標
+    # print('Wheel:',event.Wheel)              #鼠標滾輪
+    # print('Injected:',event.Injected)        #判斷這個事件是否由程序方式生成，而不是正常的人為觸發。
     # print('---')
     message = event.MessageName
     if message == 'mouse wheel':
@@ -58,7 +58,7 @@ def on_mouse_event(event):
 
     delay = globalv.current_ts() - globalv.latest_time
 
-    # 录制鼠标轨迹的精度，数值越小越精准，但同时可能产生大量的冗余
+    # 錄制鼠標軌跡的精度，數值越小越精準，但同時可能產生大量的冗余
     mouse_move_interval_ms = globalv.mouse_interval_ms or 999999
 
     if message == 'mouse move' and delay < mouse_move_interval_ms:
@@ -84,19 +84,19 @@ def on_mouse_event(event):
 
 
 def on_keyboard_event(event):
-    # print('MessageName:',event.MessageName)          #同上，共同属性不再赘述
+    # print('MessageName:',event.MessageName)          #同上，共同屬性不再贅述
     # print('Message:',event.Message)
     # print('Time:',event.Time)
     # print('Window:',event.Window)
     # print('WindowName:',event.WindowName)
-    # print('Ascii:', event.Ascii, chr(event.Ascii))   #按键的ASCII码
-    # print('Key:', event.Key)                         #按键的名称
-    # print('KeyID:', event.KeyID)                     #按键的虚拟键值
-    # print('ScanCode:', event.ScanCode)               #按键扫描码
-    # print('Extended:', event.Extended)               #判断是否为增强键盘的扩展键
+    # print('Ascii:', event.Ascii, chr(event.Ascii))   #按鍵的ASCII碼
+    # print('Key:', event.Key)                         #按鍵的名稱
+    # print('KeyID:', event.KeyID)                     #按鍵的虛擬鍵值
+    # print('ScanCode:', event.ScanCode)               #按鍵掃描碼
+    # print('Extended:', event.Extended)               #判斷是否為增強鍵盤的擴展鍵
     # print('Injected:', event.Injected)
-    # print('Alt', event.Alt)                          #是某同时按下Alt
-    # print('Transition', event.Transition)            #判断转换状态
+    # print('Alt', event.Alt)                          #是某同時按下Alt
+    # print('Transition', event.Transition)            #判斷轉換狀態
     # print('---')
 
     message = event.MessageName
@@ -142,7 +142,7 @@ def mouse_handler(msg, x, y, data, flags, time, hwnd, window_name):
 def setuphook(commandline=False):
     hm = pyWinhook.HookManager()
     if not commandline:
-        # 使用一般的HookMouse无法捕获鼠标侧键操作，因此采用cpyHook捕获鼠标操作
+        # 使用一般的HookMouse無法捕獲鼠標側鍵操作，因此采用cpyHook捕獲鼠標操作
         cpyHook.cSetHook(HookConstants.WH_MOUSE_LL, mouse_handler)
     hm.KeyAll = on_keyboard_event
     hm.HookKeyboard()
